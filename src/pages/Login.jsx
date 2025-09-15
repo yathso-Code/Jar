ed4175f87293227b601dc9c72848f006f934726f
yatharth <yatharth@immutabledevs.com>
2025-09-15 16:19:45 +0530

    change in login to Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./login.css"; // import CSS file
+import "./Login.css"; // import CSS file
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
